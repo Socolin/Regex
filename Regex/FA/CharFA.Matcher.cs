be01faa2e0357b8d90c09ac52f09127d6d05e861
Socolin <provost.bertrand@gmail.com>
2021-03-02 20:44:43 -0500

    Support partial matching of regex (Return true when input ends in valid state)

--- Regex/FA/CharFA.Matcher.cs
+++ Regex/FA/CharFA.Matcher.cs
@@ -8,9 +8,10 @@ namespace RE
 		/// <summary>
 		/// Pattern matches through a string of text
 		/// </summary>
+		/// <param name="successOnAnyState">When true, it will return success if the input string can be fully match but is incompleted. Eg: Regex('Abc') will return true for 'Ab'</param>
 		/// <param name="context">The parse context to search</param>
 		/// <returns>A <see cref="CharFAMatch"/> that contains the match information, or null if the match is not found.</returns>
-		public CharFAMatch Match(ParseContext context)
+		public CharFAMatch Match(ParseContext context, bool successOnAnyState = false)
 		{
 			context.EnsureStarted();
 			var line = context.Line;
@@ -19,7 +20,7 @@ namespace RE
 			var l = context.CaptureBuffer.Length;
 			var success = false;
 			// keep going until we find something or reach the end
-			while (-1 != context.Current && !(success = _DoMatch(context)))
+			while (-1 != context.Current && !(success = _DoMatch(context, successOnAnyState)))
 			{
 				line = context.Line;
 				column = context.Column;
@@ -98,7 +99,7 @@ namespace RE
 			return null;
 		}
 		// almost the same as our lex methods
-		bool _DoMatch(ParseContext context)
+		bool _DoMatch(ParseContext context, bool successOnAnyState = false)
 		{
 			// get the initial states
 			var states = FillEpsilonClosure();
@@ -108,7 +109,7 @@ namespace RE
 				if (-1 == context.Current)
 				{
 					// if we accept, return that
-					return IsAnyAccepting(states);
+					return successOnAnyState || IsAnyAccepting(states);
 				}
 				// move by current character
 				var newStates = FillMove(states, (char)context.Current, captureGroupHandler: state => HandleCaptureGroup(context, state));
